9cfe21ac27eaf3914e84484863c6b0b5dcb83d74
Cédric Morier-Roy <cedric@hal9001.gateway.mts.net>
2019-02-28 17:25:38 -0600

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sideForce = 500f;
+
+    // Fixed Update is used to mess with physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //force of 2000 on the z-axis
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); //force of 500 on x-axis if d is pressed
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); //force of 500 on x-axis if d is pressed
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
